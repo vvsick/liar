1542afd13b35d6359f02aa0912af75efb8f59038
vvsick <vladimirosykin@gmail.com>
2024-06-19 20:03:25 +0300

    gh pages setup

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
-  ],
+    ],
+  base: '/liar',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
